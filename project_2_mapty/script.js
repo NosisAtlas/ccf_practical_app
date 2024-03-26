5a73083339b86daf6c50cc794cdced84296ec637
nosisatlas <fz.farhane109@gmail.com>
2024-03-26 18:09:06 +0100

    Using the browser geolocation API and displaying the current location in the console

--- project_2_mapty/script.js
+++ project_2_mapty/script.js
@@ -10,3 +10,20 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+// Using the geolocation browser API
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    // success callback
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+    },
+    // fail callback
+    function () {
+      alert("Could not get your position");
+    }
+  );
