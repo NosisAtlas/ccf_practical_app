778fb5ceff02222a194c53f919e80b37fe7e1345
nosisatlas <fz.farhane109@gmail.com>
2024-03-27 22:35:05 +0100

    Challenge 19 async - Use of geocoding api to search country by coordinates

---
+++ challenge_19_async/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+// Rendering data to display it in html
+const renderCountry = function (data, className = "") {
+  const html = `<article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)}</p>
+        <p class="country__row"><span>🗣️</span>${Object.values(
+          data.languages
+        ).join(", ")}</p>
+        <p class="country__row"><span>💰</span>${
+          Object.values(data.currencies)[0].name
+        } (${Object.values(data.currencies)[0].symbol})</p>
+    </div>
+    </article>`;
+  // Inserting data code to html
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const whereAmI = function (lat, lng) {
+  fetch(
+    `https://geocode.xyz/${lat},${lng}?geoit=json&auth=492903625789883650030x62670 `
+  )
+    .then((res) => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+      return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
+    })
+    .then((res) => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+      return res.json();
+    })
+    .then((data) => renderCountry(data[0]))
+    .catch((err) => console.error(`${err.message} pupuh`));
+};
+
+// newGetCountryData("portugal");
+btn.addEventListener("click", function () {
+  whereAmI(-33.933, 18.474);
+});
