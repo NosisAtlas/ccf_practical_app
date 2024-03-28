45fd45d5ff73eb27eac3c7c16d4d21b6b48f4041
nosisatlas <fz.farhane109@gmail.com>
2024-03-28 12:24:06 +0100

    Project 3 forkify done

--- project_3_forkify/src/js/config.js
+++ project_3_forkify/src/js/config.js
@@ -1,2 +1,5 @@
-export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
-export const TIMOUT_SEC = 10;
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f0276cb8-f783-4e3d-a1a6-9247d69ba7ab';
+export const MODAL_CLOSE_SEC = 2.5;
