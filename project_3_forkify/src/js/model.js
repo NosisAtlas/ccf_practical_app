8347197bf92562cb172da9ed5d128ce8702fec19
nosisatlas <fz.farhane109@gmail.com>
2024-03-28 04:01:11 +0100

    Creating helpers and config file

--- project_3_forkify/src/js/model.js
+++ project_3_forkify/src/js/model.js
@@ -1,15 +1,14 @@
+import { API_URL } from "./config.js";
+import { getJSON } from "./helpers.js";
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      //   `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
-    );
-    const data = await res.json();
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}/5ed6604591c37cdc054bc886`);
+
     const { recipe } = data.data;
     state.recipe = {
       id: recipe.id,
@@ -23,6 +22,6 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(err);
   }
 };
